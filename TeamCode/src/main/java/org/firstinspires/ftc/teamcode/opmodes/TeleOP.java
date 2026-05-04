36090cd6d256813a6c150fe3f1ca3edba732be6e
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2026-05-03 21:57:09 -0700

    upload code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOP.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import dev.nextftc.bindings.BindingManager;
+import dev.nextftc.ftc.NextFTCOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import dev.nextftc.hardware.driving.DriverControlledCommand;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+
+import org.firstinspires.ftc.teamcode.common.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.common.Parts;
+
+@TeleOp(group = "Decode")
+public class TeleOP extends NextFTCOpMode {
+    Parts part = new Parts();
+    public TeleOP() {
+        addComponents(
+                new SubsystemComponent(Intake.INSTANCE),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        DriverControlledCommand driverControlled = new MecanumDriverControlled(
+                Parts.FL,
+                Parts.FR,
+                Parts.BL,
+                Parts.BR,
+                Gamepads.gamepad1().leftStickY().negate(),
+                Gamepads.gamepad1().leftStickX(),
+                Gamepads.gamepad1().rightStickX()
+        );
+        driverControlled.schedule();
+
+        Gamepads.gamepad1().leftBumper()
+                .whenBecomesTrue(() -> driverControlled.setScalar(0.5))
+                .whenBecomesFalse(() -> driverControlled.setScalar(1));
+
+        Gamepads.gamepad1().leftTrigger().greaterThan(0.2)
+                .whenBecomesTrue(Intake.INSTANCE.forward)
+                .whenBecomesFalse(Intake.INSTANCE.stop);
+
+        Gamepads.gamepad1().rightTrigger().greaterThan(0.2)
+                .whenBecomesTrue(Intake.INSTANCE.reverse)
+                .whenBecomesFalse(Intake.INSTANCE.stop);
+
+
+    }
+
+    @Override
+    public void onUpdate() {
+        BindingManager.update();
+    }
+
+    @Override
+    public void onStop() {
+        BindingManager.reset();
+    }
+}
