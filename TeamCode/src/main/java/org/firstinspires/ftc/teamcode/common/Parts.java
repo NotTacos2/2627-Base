bf70141b55c34abeac753bc0c722dac21ec3ef7a
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2026-05-03 22:14:02 -0700

    remove unused field centric imu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Parts.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Parts.java
@@ -1,20 +1,16 @@
 package org.firstinspires.ftc.teamcode.common;
 
-import dev.nextftc.hardware.impl.Direction;
 import dev.nextftc.hardware.impl.MotorEx;
-import dev.nextftc.hardware.impl.IMUEx;
 
 public class Parts {
     public static MotorEx FR, FL, BR, BL;
     public static MotorEx intake;
-    public static IMUEx imu;
 
     public Parts() {
         FL = new MotorEx("lf").reversed().brakeMode();
         FR = new MotorEx("rf").brakeMode();
         BR = new MotorEx("rr").brakeMode();
         BL = new MotorEx("lr").reversed().brakeMode();
-        imu = new IMUEx("imu", Direction.UP, Direction.RIGHT).zeroed();
         intake = new MotorEx("intake").floatMode();
     }
 }
